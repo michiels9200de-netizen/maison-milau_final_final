b0f3a2e69604f33fb39aed3b2c181b2e36aa860e
michiels9200de-netizen <michiels.9200de@gmail.com>
2026-09-04 23:23:44 +0000

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../server.ts';
+import app from '../server.';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return app(req, res);
